8ad43318e53433f84b6fda9a0ea809d22d078fdb
Alexandre Paez <alepaezseq@gmail.com>
2013-07-24 23:05:22 -0300

    Contact Api V1 Tests

---
+++ test/controllers/api/v1/product_controller_test.rb
@@ -0,0 +1,28 @@
+
+require "minitest_helper"
+
+class Api::V1::ProductControllerTest < ActionController::TestCase
+  before do
+    prepare_default_env
+  end
+
+  it 'true' do
+    assert true
+  end
+
+  describe "Index" do
+  end
+
+  describe "Show" do
+  end
+
+  describe "Create" do
+  end
+
+  describe "Update" do
+  end
+
+  describe "Destroy" do
+  end
+
+end
