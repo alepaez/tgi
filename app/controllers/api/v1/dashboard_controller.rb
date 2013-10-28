c5503e4953af6f8ae68cd47dba9f063f2956594e
Alexandre Paez <alepaezseq@gmail.com>
2013-10-28 12:13:57 -0200

    Fix Dashboard

--- app/controllers/api/v1/dashboard_controller.rb
+++ app/controllers/api/v1/dashboard_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::DashboardController < Api::V1::BaseController
   def recent_income
     @income_last_week = graphic_gen.income_last_week
     @income_this_week = graphic_gen.income_this_week
-    @growth_percent = @income_last_week == 0 ? 0 : ((@income_this_week-@income_last_week)/@income_last_week)*100
+    @growth_percent = @income_last_week == 0 ? 0 : (((@income_this_week-@income_last_week)/@income_last_week.to_f)*100).round(2)
     render json: {last_week: "R$#{Money.new(@income_last_week, 'BRL')}", this_week: "R$#{Money.new(@income_this_week, 'BRL')}", growth_percent: @growth_percent }
   end
 
